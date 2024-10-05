9e5c95884e1a9ae9abc67c28ad2a1f29634ebf92
Ean <eamt89@gmail.com>
2024-10-04 21:32:45 -0400

    invalidation add 3

--- output.tf
+++ output.tf
@@ -12,6 +12,6 @@ output "custom_domain" {
 }
 
 output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.your_distribution.id
+  value = aws_cloudfront_distribution.s3_distribution.id
   description = "The ID of the CloudFront distribution."
 }
