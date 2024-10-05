d011abfccf7901fd952ed9e306c4fe2e61a03fda
Ean <eamt89@gmail.com>
2024-10-04 21:19:59 -0400

    invalidation add 2

--- output.tf
+++ output.tf
@@ -10,3 +10,8 @@ output "cloudfront_distribution_domain_name" {
 output "custom_domain" {
   value = "terracloudrlm.com"
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.your_distribution.id
+  description = "The ID of the CloudFront distribution."
+}
